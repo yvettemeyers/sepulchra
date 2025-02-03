875c754722db8c1aa8dea53f603e47a57840f767
Yvette Meyers <makepr3tend@gmail.com>
2025-02-02 20:27:35 -0600

    Quartz sync: Feb 2, 2025, 8:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,11 +30,11 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
+          light: "#282828",
+          lightgray: "#3c3836",
+          gray: "#3c3836",
+          darkgray: "#fbf1c7",
+          dark: "#cc241d",
           secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",

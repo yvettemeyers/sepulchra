2571e7cab1453be629e6c0a4050458757ec202ed
Yvette Meyers <makepr3tend@gmail.com>
2025-02-02 19:55:45 -0600

    Quartz sync: Feb 2, 2025, 7:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Sepulchra",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
